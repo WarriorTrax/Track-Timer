bf4796363a8876e21b47a184626721ab53771666
WarriorTrax <warriortrax@gmail.com>
2026-05-03 21:45:09 +0000

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Track-Timer/"
+  base: "/Track-Timer/"   // MUST match repo name exactly
 });
