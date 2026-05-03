717693204faa9c0a13fbcd726e64d463d1cb0668
WarriorTrax <warriortrax@gmail.com>
2026-05-03 21:49:03 +0000

    another fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,7 @@
-import React, { useState } from "react";
-import Operator from "./views/Operator";
-import Scoreboard from "./views/Scoreboard";
-import ScheduleBuilder from "./views/ScheduleBuilder";
-import TeamLeaderboard from "./views/TeamLeaderboard";
-
 export default function App() {
-  const [view, setView] = useState("operator");
-
-  if (view === "scoreboard") return <Scoreboard setView={setView} />;
-  if (view === "schedule") return <ScheduleBuilder setView={setView} />;
-  if (view === "teams") return <TeamLeaderboard setView={setView} />;
-
-  return <Operator setView={setView} />;
+  return (
+    <div style={{ color: "white", background: "black", padding: 20 }}>
+      Track Timer is Working
+    </div>
+  );
 }
