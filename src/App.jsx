b255094e9c4fe56af3fd1d2a743f41aaa353c00a
WarriorTrax <warriortrax@gmail.com>
2026-05-03 21:52:49 +0000

    another dix

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 export default function App() {
   return (
-    <div style={{ color: "white", background: "black", padding: 20 }}>
-      Track Timer is Working
+    <div style={{
+      color: "white",
+      background: "black",
+      height: "100vh",
+      padding: 20
+    }}>
+      TRACK TIMER IS WORKING
     </div>
   );
 }
